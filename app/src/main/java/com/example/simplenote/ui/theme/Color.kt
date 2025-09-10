ca42a2160b1f7d25a71be6e5b671d52ac529246d
Ali Majidi <alimajidi1.work@gmail.com>
2025-09-10 11:31:48 +0000

    Revert "Implement Home screen with note display, search functionality, and empty state handling; add supporting components for navigation and UI"
    This reverts commit 236f1f40

--- app/src/main/java/com/example/simplenote/ui/theme/Color.kt
+++ app/src/main/java/com/example/simplenote/ui/theme/Color.kt
@@ -2,18 +2,7 @@ package com.example.simplenote.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Background = Color(0xFFFAF8FC)
-val PrimaryAccent = Color(0xFF504EC3)
-val TextMain = Color(0xFF180E25)
-val TextSecondary = Color(0xFF827D89)
-val PlaceholderText = Color(0xFFC8C5CB)
+val Primary = Color(0xFF504EC3)
+val Secondary = Color(0xFFDEDC52)
 val NeutralWhite = Color(0xFFFFFFFF)
-val FunctionalLightGray = Color(0xFFEFEEF0)
-val NoteCardYellow = Color(0xFFF7F6D4)
-val NoteCardPeach = Color(0xFFFDEBAB)
-val NoteCardRose = Color(0xFFF7DEE3)
-val TabBarBackground = Background
-val TabBarIconInactive = TextSecondary
-val TabBarIconActive = PrimaryAccent
-val ArrowAccent = PrimaryAccent
-val StatusBarText = TextMain
+val NeutralBlack = Color(0xFF180E25)
